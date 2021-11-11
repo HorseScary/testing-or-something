2051329e45d1fe835a720cfe8d401d89aab656b9
HorseScary <horsescary@gmail.com>
2021-11-10 22:50:56 -0500

    made a new file to learn recursio nin (but this time im actualy using a tutorial)

---
+++ recursion-real.py
@@ -0,0 +1,4 @@
+"""
+Unlike my other test, I am actually using outside resources to try and learn recursion
+This is the site im looking at: https://www.cs.utah.edu/~germain/PPS/Topics/recursion.html
+"""
