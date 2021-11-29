645d441938361dfb8ab6478e541aec96c8181936
horse <horsescary@gmail.com>
2021-11-29 07:38:45 -0500

    stuff n things

--- brainfuck/brainfuck.py
+++ brainfuck/brainfuck.py
@@ -5,45 +5,43 @@ An attempt at writing a brainfuck interprater
 def fuck(pro):
     cell = [0]
     program = []
-    pos = 0
+    pointer = 0
     open = 0
     loop = []
+
+
     for i in range(len(pro)):
         program.append(pro[i])
 
     for i in program:
         if i == '>':
-            pos += 1
+            pointer += 1
             try:
-                cell[pos]
+                cell[pointer]
             except IndexError:
                 cell.append(0)
 
         elif i == '<':
-            if pos == 0:
+            if pointer == 0:
                 cell.insert(0,0)
             else:
-                pos -= 1
+                pointer -= 1
 
         elif i == '+':
-            cell[pos] += 1
+            cell[pointer] += 1
 
         elif i == '-':
-            cell[pos] -= 1
+            cell[pointer] -= 1
 
         elif i == '[':
             open += 1
 
         elif i == ']':
             open -= 1
-
-            if pos == 0:
+            if cell[pointer] == 0:
                 continue
-            else:
-                fuck(pro)
-
         elif i == '.':
-            print(cell[pos], end='')
+            print(cell[pointer], end='')
 
         else:
             continue
@@ -51,6 +49,6 @@ def fuck(pro):
     if open != 0:
         return('LoopError\nYour loops are fucked up')
 
-    return(f'\n{open}')
+    return(f'\n{cell}')
 
-print(fuck("+++[>+<-]>."))
+print(fuck("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."))
