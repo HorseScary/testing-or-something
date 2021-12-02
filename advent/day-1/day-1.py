0caf1fc7bd13cd7a11cc87400c34ab8e6a22656a
horse <horsescary@gmail.com>
2021-12-02 09:20:46 -0500

    finished 2nd problem day 1

--- advent/day-1/day-1.py
+++ advent/day-1/day-1.py
@@ -9,6 +9,19 @@ def sonar (data):
 
     return(count)
 
+def sonar2(data):
+    count = 0
+
+    for i in range (len(data)):
+        try:
+            a = data[i] + data[i+1] + data[i+2]
+            b = data[i+1] + data[i+2] + data[i+3]
+        except IndexError:
+            continue
+        if a < b:
+            count += 1
+    return(count)
+
 def read(file):
     data = []
     readData = open(file, 'r')
@@ -18,4 +31,4 @@ def read(file):
 
     return(data)
 
-print(sonar(read('advent/day-1/data.txt')))
+print(sonar2(read('advent/day-1/data.txt')))
