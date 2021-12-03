334037a23978dcea36887b6f2f159b60527dca79
horse <horsescary@gmail.com>
2021-12-03 10:18:48 -0500

    matchingparen actual returns start and end positions

--- brainfuck/matchParen.py
+++ brainfuck/matchParen.py
@@ -2,7 +2,7 @@ def match(toMatch):
     i = 0
 
     matching = []
-    open = 0
+    open = [0, 0]
     paren = []
 
     for i in range(len(toMatch)):
@@ -10,23 +10,26 @@ def match(toMatch):
         matching.append(a)
         matching[i].insert(0, i)
     #tbh this should probably be a for loop
+    i = 0
     
     while i < len(matching):
         if matching[i][1] == '[':
-            open = matching[i][0]
+            open[1] = matching[i][0]
+            open[0] = i
 
         elif matching[i][1] == ']':
-            paren.append([open, matching[i][0]])
+            paren.append([open[1], matching[i][0]])
             del(matching[i])
+            del(matching[open[0]])
             i = 0
-            print('Close')
+        
         i += 1
     
     return(paren)
-
 
 a = ['+', '+', '+', '+', '+', '+', '+', '+', '[', '>', '+', '+', '+', '+', '[', '>', '+', '+', '>', '+', '+', '+', '>', '+', '+', '+', '>', '+', '<', '<', '<', '<', '-', ']', '>', '+', '>', '+', '>', '-', '>', '>', '+', '[', '<', ']', '<', '-', ']', '>', '>', '.', '>', '-', '-', '-', '.', '+', '+', '+', '+', '+', '+', '+', '.', '.', '+', '+', '+', '.', '>', '>', '.', '<', '-', '.', '<', '.', '+', '+', '+', '.', '-', '-', '-', '-', '-', '-', '.', '-', '-', '-', '-', '-', '-', '-', '-', '.', '>', '>', '+', '.', '>', '+', '+', '.']
 print (match(a))
 
 #expected output: 
+# [8, 48] [14, 33] [43, 45]
 # [0, 6] [2, 3] [4, 5]
