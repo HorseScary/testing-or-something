13b2b455a0ca88f6c981df739ee7cefdf603aa4d
Vi <panagen3002@gmail.com>
2022-12-08 11:22:03 -0500

    added mode validator

--- dirtquiz.py
+++ dirtquiz.py
@@ -13,7 +13,19 @@ dirts = list(dirts.items())
 dirts_correct = 0
 dirts_wrong = 0
 print("Welcome to the HorseScary dirt quiz!\nmode 1: gives dirt location, asks for dirt type\nmode 0: gives dirt type, asks for location")
-mode = bool(int(input("mode: ")))
+
+while True:
+    mode = input("mode: ")
+    try:
+        mode = int(mode)
+    except ValueError:
+        print("Invalid input. Please enter 0 or 1")
+        continue
+
+    if mode > 1:
+        print("Invalid mode. Valid modes are 0 and 1.")
+        continue
+    break
 
 while True:
     if len(dirts) == 0:
